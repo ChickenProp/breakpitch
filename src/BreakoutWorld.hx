5a155e420ef02f0bcd86bca851090c2186416ecd
Philip Hazelden <philip.hazelden@gmail.com>
2012-06-23 05:17:41 +0100

    Paddle moves about in a bounded world.

--- src/BreakoutWorld.hx
+++ src/BreakoutWorld.hx
@@ -1,7 +1,30 @@
 import com.haxepunk.World;
+import com.haxepunk.HXP;
+import com.haxepunk.utils.Draw;
 
 class BreakoutWorld extends World {
+	public var width:Int;
+	public var height:Int;
+	public var left(getLeft, null):Float;
+	public var right(getRight, null):Float;
+	public var top(getTop, null):Float;
+
 	override public function begin () : Void {
 		add(new Paddle());
+		width = 560;
+		height = 440;
 	}
+
+	override public function render () : Void {
+		Draw.rect(Std.int( (HXP.width - width)/2 ),
+		          HXP.height - height,
+		          width, height,
+		          0xCCCCFF);
+
+		super.render();
+	}
+
+	function getLeft () : Float { return (HXP.width - width) / 2; }
+	function getRight () : Float { return (HXP.width + width) / 2; }
+	function getTop () : Float { return HXP.height - height; }
 }
--- src/Paddle.hx
+++ src/Paddle.hx
@@ -1,8 +1,12 @@
 import com.haxepunk.Entity;
 import com.haxepunk.HXP;
 import com.haxepunk.utils.Draw;
+import com.haxepunk.utils.Input;
+import com.haxepunk.utils.Key;
 
 class Paddle extends Entity {
+	var vel:Float;
+
 	public function new () {
 		super();
 
@@ -11,6 +15,28 @@ class Paddle extends Entity {
 		width = 40;
 		height = 10;
 		centerOrigin();
+
+		vel = 0;
+	}
+
+	override public function update () : Void {
+		var dx = (if (Input.check(Key.RIGHT)) 1 else 0)
+			- (if (Input.check(Key.LEFT)) 1 else 0);
+
+		vel += 3 * dx;
+		vel *= 0.9;
+		x += vel;
+
+		var bw = cast(world, BreakoutWorld);
+		if (x - halfWidth < bw.left) {
+			x = bw.left + halfWidth;
+			vel = 0.9 * Math.abs(vel);
+		}
+		else if (x + halfWidth > bw.right) {
+			x = bw.right - halfWidth;
+			vel = -0.9 * Math.abs(vel);
+		}
+
 	}
 
 	override public function render () : Void {
