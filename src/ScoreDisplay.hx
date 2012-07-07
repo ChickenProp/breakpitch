c3426012af4134db1fbe1a120e22dfc65e6a5089
Philip Hazelden <philip.hazelden@gmail.com>
2012-07-08 00:04:09 +0100

    Displayed score increases gradually.
    Might make sense to accelerate if the displayed score is far behind
    actual score? But that's probably only a problem when we cheat.

--- src/ScoreDisplay.hx
+++ src/ScoreDisplay.hx
@@ -4,17 +4,26 @@ import com.haxepunk.graphics.Text;
 class ScoreDisplay extends Entity {
 	var score:Text;
 	var hiscore:Text;
+	var dispScore:Int;
+	var dispHiscore:Int;
 	public function new () {
 		super();
 		score = new Text("                  ", 350, 10);
 		hiscore = new Text("                   ", 470, 10);
 		addGraphic(score);
 		addGraphic(hiscore);
+		dispScore = 0;
+		dispHiscore = G.hiscore;
 	}
 
 	override public function update () : Void {
-		score.text = Std.format("Score: ${G.score}");
-		hiscore.text = Std.format("HiScore: ${G.hiscore}");
+		if (dispScore < G.score)
+			dispScore++;
+		if (dispHiscore < dispScore)
+			dispHiscore = dispScore;
+
+		score.text = Std.format("Score: $dispScore");
+		hiscore.text = Std.format("HiScore: $dispHiscore");
 		super.update();
 	}
 }
